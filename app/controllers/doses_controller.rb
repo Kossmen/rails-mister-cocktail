dd3425c2744102fbc52f030528485cd5ffa0c13e
Leonhard Kossmann <leonhardkossmann@gmail.com>
2019-11-15 12:37:12 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,9 +8,11 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail_id = @cocktail.id
 
     if @dose.save
-      redirect_to cocktail_path(@dose.cocktail_id)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
@@ -27,6 +29,6 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
